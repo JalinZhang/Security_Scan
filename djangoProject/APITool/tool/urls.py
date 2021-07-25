fbe4be9fb4a449dd5107152f6b4d631becce8659
0027013841 <0027013841@iwhalecloud.com>
2021-07-25 15:00:54 +0800

    init

--- djangoProject/APITool/tool/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-from tool import views
-
-urlpatterns = {
-    path('type_list/', views.type_list),
-}
-
-
-
--- djangoProject/APITool/tool/views.py
+++
@@ -1,9 +0,0 @@
-from django.shortcuts import render, HttpResponse
-from tool import models
-
-
-def type_list(request):
-    type_all = models.Type.objects.all()
-    return render(request,'type_list.html', {'type_all': type_all})
-
-# Create your views here.
